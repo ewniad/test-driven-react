2dbf825e74e723929bc0f09c375389d48337db7b
e.wakabayashi <eijirou.wakabayashi@unictgroup.com>
2021-12-28 15:54:57 +0900

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    plugins: ['react'],
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parser: 'babel-eslint',
+    env: {
+        node: true,
+    },
+    rules: {
+        quotes: ['error', 'single', { avoidEscape: true }],
+        'comma-dangle': ['error', 'always-multiline'],
+    },
+    settings: {
+        react: {
+            version: '16.4.2',
+        },
+    },
+};
